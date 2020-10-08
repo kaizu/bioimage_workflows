810f57c055862269e1ee54ae01a8357a3afb483c
Kozo Nishida <kozo.nishida@gmail.com>
2020-10-08 15:07:00 +0900

    Use papermill again

--- main.py
+++ main.py
@@ -12,33 +12,33 @@ with mlflow.start_run(nested=True):
    artifacts = pathlib.Path("./artifacts")
    artifacts.mkdir(parents=True, exist_ok=True)
 
-#    import papermill as pm
-#    _ = pm.execute_notebook(
-#       'generation.ipynb',
-#       str(artifacts / 'generation.ipynb'),
-#       parameters=dict(num_samples=num_samples, num_frames=num_frames)
-#    )
-   exec(open("generation.py").read())
+   import papermill as pm
+   _ = pm.execute_notebook(
+      'generation.ipynb',
+      str(artifacts / 'generation.ipynb'),
+      parameters=dict(num_samples=num_samples, num_frames=num_frames)
+   )
+   #exec(open("generation.py").read())
 
-#    _ = pm.execute_notebook(
-#       'analysis1.ipynb',
-#       str(artifacts / 'analysis1.ipynb'),
-#       parameters=dict(num_samples=num_samples, num_frames=num_frames)
-#    )
-   exec(open("analysis1.py").read())
+   _ = pm.execute_notebook(
+      'analysis1.ipynb',
+      str(artifacts / 'analysis1.ipynb'),
+      parameters=dict(num_samples=num_samples, num_frames=num_frames)
+   )
+   #exec(open("analysis1.py").read())
 
-#    _ = pm.execute_notebook(
-#       'analysis2.ipynb',
-#       str(artifacts / 'analysis2.ipynb'),
-#       parameters=dict(num_samples=num_samples, num_frames=num_frames)
-#    )
-   exec(open("analysis2.py").read())
+   _ = pm.execute_notebook(
+      'analysis2.ipynb',
+      str(artifacts / 'analysis2.ipynb'),
+      parameters=dict(num_samples=num_samples, num_frames=num_frames)
+   )
+   #exec(open("analysis2.py").read())
 
    log_artifacts("./artifacts")
 
-#    _ = pm.execute_notebook(
-#       'evaluation1.ipynb',
-#       str(artifacts / 'evaluation1.ipynb'),
-#       parameters=dict(num_samples=num_samples)
-#    )
-   exec(open("evaluation1.py").read())
+   _ = pm.execute_notebook(
+      'evaluation1.ipynb',
+      str(artifacts / 'evaluation1.ipynb'),
+      parameters=dict(num_samples=num_samples)
+   )
+   #exec(open("evaluation1.py").read())
