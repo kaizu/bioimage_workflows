e3c76585347d8f880d01a010e1ecc361fd75e933
kaizu <kwaizu@gmail.com>
2020-10-23 16:46:12 +0900

    Ref locals

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import argparse
 
 import mlflow
 from mlflow import log_metric, log_param, log_artifacts
-from .mlflow_utils import _get_or_run
+from mlflow_utils import _get_or_run
 
 """Prepare for generating inputs."""
 parser = argparse.ArgumentParser(description='analysis1 step')
